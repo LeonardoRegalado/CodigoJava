8083daaf1353d05fe79de2718364b665776cb501
Leonardo <leonardoalev@gmail.com>
2025-02-05 20:19:54 -0600

    Utilizando ArrayList

--- Animal.java
+++
@@ -1,8 +0,0 @@
-
-package javaCode;
-public class Animal{
-
-    public Animal(){
-        System.err.println("Este es un animal");
-    }
-}
--- pruebas/main.java
+++ pruebas/main.java
@@ -1,5 +1,6 @@
 package pruebas;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -9,21 +10,53 @@ abstract class main {
     public static int antiguedad;
 
 
-    public static void main(String[] args) {
-        String[] bancos = {"NU","BBVA","SCOTIA BANK","BANREJIO","SANTANDER"};
-        String palabra = "Esta es una prueba";
-        
-        //palabra[0] = "Hol";
-        for (char letra : palabra.toCharArray()) {
-            System.out.println(letra);
-        }
-        System.out.println("Lista de bancos");
-        for (String banco : bancos) {
-            System.out.println(banco);
-        }
+    public static void main(String[] args) {                                        
+    String[] bancos = {"NU","BBVA","SCOTIA BANK","BANREJIO","SANTANDER"};
+    String palabra = "Esta es una prueba";
+    char letra = 'a';
+    //palabra[0] = "Hol";
+    /*
+        System.out.println("Longitud de un arreglo "+bancos.length);
+    System.out.println("Longitud de una poalabra "+palabra.length());
+    for (char letra : palabra.toCharArray()) {
+        System.out.println(letra);
+    }
+    System.out.println("Lista de bancos");
+    for (String banco : bancos) {
+        System.out.println(banco);
+    }
+        */
+    String[] copia = bancos.clone();
+    //bancos[0] = "BANCOMER";
+    //System.out.println(copia[0]); NU
+    //System.out.println(bancos[0]); BANCOMER
+    //System.out.println(Arrays.equals(bancos, copia)); // Devuelve true
+    System.out.println();
 
+    List<String> palabras = new LinkedList<>();
+    
 
-        List<String> palabras = new LinkedList<>();
+    ArrayList<String> cars = new ArrayList<String>();
+    cars.add("Volvo");
+    cars.add("BMW");
+    cars.add("Ford");
+    cars.add("Mazda");
+    
+    ArrayList cars2 = (ArrayList)cars.clone();
+    cars.set(0, "Toyota");
+    
+    System.out.println(cars);
+    System.out.println(cars2);
         
+    ArrayList <Integer> numeros = new ArrayList<>();
+        numeros.add(2);
+        numeros.add(6);
+        numeros.add(25);
+        numeros.add(4);
+        numeros.add(233);
+        numeros.add(11);
+    numeros.forEach((numero)-> {
+        System.out.println(numero + 1);
+    });
     }
 }
