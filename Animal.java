69452fd1c801bd35726e80da688da78421bcf981
Leonardo <leonardoalev@gmail.com>
2025-02-05 23:11:41 -0600

    Agregue comentario a Animal.java

---
+++ Animal.java
@@ -0,0 +1,10 @@
+
+
+public class Animal{
+
+    public Animal(){
+        System.err.println("Este es un animal");
+        //Esta sera una prueba paara ver que pasa si hago un git checkout en el caso
+        // donde haga un git checkout sin haber hecho commit de los nuevos cambios
+    }
+}
